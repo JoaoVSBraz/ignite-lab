13d899396df5f9278bf25cc213775af451f9f56a
JoaoVSBraz <joaovictor3.14@outlook.com>
2022-06-25 17:21:31 -0300

    deploying app

---
+++ src/pages/Event.tsx
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
+import { Videoplayer } from "../components/Videoplayer";
+
+export function Event() {
+  const { slug } = useParams<{ slug: string }>()
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        {slug ? <Videoplayer lessonSlug={slug} /> : <div className="flex-1"></div>}
+        <Sidebar />
+      </main>
+    </div>
+  )
+}
